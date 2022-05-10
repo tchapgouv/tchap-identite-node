727f43ab7f5311dcde6c44387f709e1658b4d8e9
Calev Eliacheff <c.eliacheff@gmail.com>
2022-05-10 16:41:08 +0200

    typo

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-const appUrl: string = process.env.APP_URL ? process.env.APP_URL.replace(/\/+$/, '') || `http://localhost:${port}`;
+const appUrl: string = process.env.APP_URL ? process.env.APP_URL.replace(/\/+$/, '') : `http://localhost:${port}`;
 
 export {
     port, appUrl
